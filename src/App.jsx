364ccb0814d251308615aa61ce5df9bad44189dd
Aleksei Iakovets <aleksei@winlx.com>
2017-05-31 19:35:54 +0300

    Intro has been studied

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <div>
-      <h1>React</h1>
+  const app = (
+    <div className="app">
+      <h1 className="app__title">React basics</h1>
+      <p className="app__article">Learning React basics</p>
+      <p>{new Date().toLocaleString('ru-RU', { hour: '2-digit', minute: '2-digit', second: '2-digit' })}</p>
     </div>
   );
+
+  ReactDOM.render(app, document.getElementById('root'));
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+setInterval(App, 1000);
